edb876eb90bddf2a4338dbb3c76ad30981e377da
Higor <higorribeiroaraujo007@gmail.com>
2025-07-14 23:48:42 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ class App {
 
   middlewares() {
     this.app.use(cors({
-      origin: ['*'],
+      origin: ['http://localhost:3000'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       credentials: true,
     }));
