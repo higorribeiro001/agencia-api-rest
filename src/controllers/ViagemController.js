912a6cb602186ff2ebbffd9a516f17d4dc3876a8
Higor <higorribeiroaraujo007@gmail.com>
2025-07-14 21:16:56 -0300

    fix: ajuste no swagger adicionando o body do post e patch

--- src/controllers/ViagemController.js
+++ src/controllers/ViagemController.js
@@ -22,6 +22,23 @@ class ViagemController {
   async store(req, res) {
     // #swagger.tags = ['Viagens']
     // #swagger.description = 'Cria uma nova viagem'
+    /*
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: {
+          titulo: 'Rio de Janeiro',
+          descricao: 'Viagem para o Rio com hotel incluso',
+          dias: 5,
+          valor: 1500.0,
+          avaliacao: 4,
+          latitude: -22.9035,
+          longitude: -43.2096,
+          data: '2025-10-02',
+          vagas: 100
+        }
+      }
+    */
     try {
       const trip = await Viagem.create(req.body);
 
@@ -57,6 +74,24 @@ class ViagemController {
   async update(req, res) {
     // #swagger.tags = ['Viagens']
     // #swagger.description = 'Edita uma viagem pelo seu ID'
+    /*
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: {
+          id: '94c67a6b-ccbb-4ab3-a683-3ce924592aa0',
+          titulo: 'Rio de Janeiro',
+          descricao: 'Viagem para o Rio com hotel incluso',
+          dias: 5,
+          valor: 1500.0,
+          avaliacao: 4,
+          latitude: -22.9035,
+          longitude: -43.2096,
+          data: '2025-10-02',
+          vagas: 100
+        }
+      }
+    */
     try {
       const { id } = req.params;
 
--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const port = process.env.APP_PORT;
 app.listen(port, () => {
   console.log();
   console.log(`Escutando na porta ${port}`);
-  console.log(`CTRL + Clique em ${process.env.APP_URL}:${port}/docs`);
+  console.log(`CTRL + Clique em ${process.env.APP_URL}/docs`);
 })
