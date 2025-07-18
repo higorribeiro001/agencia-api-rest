389a0aeea6d6f0616e6d541e30d8337592bf447b
Higor <higorribeiroaraujo007@gmail.com>
2025-07-17 22:48:49 -0300

    fix: ajuste na url para o cors

--- dist/app.js
+++ dist/app.js
@@ -22,7 +22,7 @@ class App {
 
   middlewares() {
     this.app.use(_cors2.default.call(void 0, {
-      origin: ['http://localhost:3000'],
+      origin: ['https://front-agencia-chi.vercel.app/'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       credentials: true,
     }));
